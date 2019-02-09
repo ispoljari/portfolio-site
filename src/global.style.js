05b085b1ca13899ba4e585db272a17bf5b000607
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-09 12:24:41 +0100

    Change base font to Palatino

--- src/global.style.js
+++ src/global.style.js
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
+    font-family: 'Palatino';
     color: rgba(250, 250, 250, 0.6);
   }
 `;
