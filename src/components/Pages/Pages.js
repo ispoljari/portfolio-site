d27db0b5de257342b1adbc0406a8f4857b023b93
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-13 16:20:40 +0100

    Adjust page transition tymes for smoother effect

--- src/components/Pages/Pages.js
+++ src/components/Pages/Pages.js
@@ -12,7 +12,7 @@ const Pages = ({ location }) => (
     >
       <CSSTransition
         key={location.key}
-        timeout={{ enter: 150, exit: 150 }}
+        timeout={{ enter: 200, exit: 100 }}
         classNames="fade"
       >
         <section
--- src/global.style.js
+++ src/global.style.js
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Lato', Arial, Helvetica, sans-serif;
     background-color: ${props => props.theme.backgroundColor};
-    transition: background-color 300ms linear;
+    transition: background-color 200ms linear;
     color: rgba(250, 250, 250, 0.6);
     letter-spacing: 1px;
     -webkit-font-smoothing: antialiased;
