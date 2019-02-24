b97eefc30154e152d9696b4a9631c40ce46ab3a9
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-24 16:15:15 +0100

    Move the dot after the word Zagreb to the same span element

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -82,9 +82,9 @@ const Home = () => (
                 href="https://www.google.com/maps/place/Zagreb/@45.8401104,15.8242483,11z/data=!3m1!4b1!4m5!3m4!1s0x4765d692c902cc39:0x3a45249628fbc28a!8m2!3d45.8150108!4d15.9819189"
                 onClick={delayLinkOpen}
               >
-                Zagreb
+                Zagreb.
               </a>
-            </span>.
+            </span>
           </Styled.Heading>
         </Box>
       </Col>
