503ac7bb48fd390974d41bceb21d2905293e73f2
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-26 09:16:52 +0100

    Remove animations from portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -33,305 +33,245 @@ const Portfolio = () => (
         as="main"
         role="main"
       >
-        <ScrollAnimation
-          animateIn="fadeInDown"
-          offset={0}
-          duration={0.4}
-          animateOnce
+        <Row
+          mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
         >
-          <Row
-            mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
+          <Col
+            xs={12}
+            mb={{xs:"15px", xl:"0px"}}
+            xl={6}
+            display= "flex"
+            alignItems= "flex-end"
           >
-            <Col
-              xs={12}
-              mb={{xs:"15px", xl:"0px"}}
-              xl={6}
-              display= "flex"
-              alignItems= "flex-end"
+            <img 
+              src={runTrackerPosts} 
+              alt="Run Tracker app screenshot"
+              style={{
+                width:"100%",
+                display: "block",
+                margin: "0 auto",
+                borderRadius: "5px"
+              }}
+            />
+          </Col>
+          <Col
+            xs={12}
+            xl={6}
+            pl={{xl:"30px"}}
+          >
+            <Styled.SectionHeading>
+              Run Tracker
+            </Styled.SectionHeading>     
+            <Styled.UsedTech
+              display="flex"
+              mt="5px"
             >
-              <img 
-                src={runTrackerPosts} 
-                alt="Run Tracker app screenshot"
-                style={{
-                  width:"100%",
-                  display: "block",
-                  margin: "0 auto",
-                  borderRadius: "5px"
-                }}
-              />
-            </Col>
-            <Col
-              xs={12}
-              xl={6}
-              pl={{xl:"30px"}}
-            >
-              <ScrollAnimation
-                animateIn="fadeInDown"
-                offset={0}
-                duration={0.4}
-                delay={400}
-                animateOnce
+              <Javascript />
+              <Html />
+              <Css />
+              <Node />
+              <Mongodb />
+              <Heroku />
+            </Styled.UsedTech>
+            <Styled.Paragraph>
+            The app allows users to log their running activity and share it with others on the platform. The design is retro-cartoonish, inspired by glitch.com.
+            </Styled.Paragraph>
+            <Styled.ProjectLinks>
+              <a 
+                href="https://github.com/ispoljari/run-tracker-app"
+                onClick={delayLinkOpen}
               >
-                <Styled.SectionHeading>
-                  Run Tracker
-                </Styled.SectionHeading>
-              </ScrollAnimation>          
-              <Styled.UsedTech
-                display="flex"
-                mt="5px"
+                View Code
+              </a>
+              <a 
+                href="https://pure-peak-20096.herokuapp.com/"
+                onClick={delayLinkOpen}
               >
-                <Javascript />
-                <Html />
-                <Css />
-                <Node />
-                <Mongodb />
-                <Heroku />
-              </Styled.UsedTech>
-              <Styled.Paragraph>
-              The app allows users to log their running activity and share it with others on the platform. The design is retro-cartoonish, inspired by glitch.com.
-              </Styled.Paragraph>
-              <Styled.ProjectLinks>
-                <a 
-                  href="https://github.com/ispoljari/run-tracker-app"
-                  onClick={delayLinkOpen}
-                >
-                  View Code
-                </a>
-                <a 
-                  href="https://pure-peak-20096.herokuapp.com/"
-                  onClick={delayLinkOpen}
-                >
-                  View Project
-                </a>
-              </Styled.ProjectLinks>
-            </Col>
-          </Row>
-        </ScrollAnimation>
-        <ScrollAnimation
-          animateIn="fadeInLeft"
-          offset={150}
-          duration={0.4}
-          animateOnce
+                View Project
+              </a>
+            </Styled.ProjectLinks>
+          </Col>
+        </Row>
+        <Styled.Hr />
+        <Row
+          mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
         >
-          <Styled.Hr />
-          <Row
-            mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
+          <Col
+            xs={12}
+            mb={{xs:"15px", xl:"0px"}}
+            xl={6}
+            display= "flex"
+            alignItems= "flex-end"
           >
-            <Col
-              xs={12}
-              mb={{xs:"15px", xl:"0px"}}
-              xl={6}
-              display= "flex"
-              alignItems= "flex-end"
+            <img 
+              src={newsDigestResults} 
+              alt="News digest app screenshot"
+              style={{
+                width:"100%",
+                display: "block",
+                margin: "0 auto",
+                borderRadius: "5px"
+              }}
+            />
+          </Col>
+          <Col
+            xs={12}
+            xl={6}
+            pl={{xl:"30px"}}
+          >     
+            <Styled.SectionHeading>
+              News Digest
+            </Styled.SectionHeading>  
+            <Styled.UsedTech
+              display="flex"
+              mt="5px"
             >
-              <img 
-                src={newsDigestResults} 
-                alt="News digest app screenshot"
-                style={{
-                  width:"100%",
-                  display: "block",
-                  margin: "0 auto",
-                  borderRadius: "5px"
-                }}
-              />
-            </Col>
-            <Col
-              xs={12}
-              xl={6}
-              pl={{xl:"30px"}}
-            >     
-               <ScrollAnimation
-                animateIn="fadeInLeft"
-                offset={150}
-                duration={0.4}
-                delay={400}
-                animateOnce
+              <Javascript />
+              <Jquery />
+              <Html />
+              <Css />
+              <GithubPages/>
+            </Styled.UsedTech>
+            <Styled.Paragraph>
+            With this app the user can search through millions of articles from various news sources and blogs, and filter them by selecting different categories.
+            </Styled.Paragraph>
+            <Styled.ProjectLinks>
+              <a 
+                href="https://github.com/ispoljari/news-digest-app"
+                onClick={delayLinkOpen}
               >
-                <Styled.SectionHeading>
-                  News Digest
-                </Styled.SectionHeading>
-              </ScrollAnimation>     
-              <Styled.UsedTech
-                display="flex"
-                mt="5px"
+                View Code
+              </a>
+              <a 
+                href="https://ispoljari.github.io/news-digest-app/"
+                onClick={delayLinkOpen}
               >
-                <Javascript />
-                <Jquery />
-                <Html />
-                <Css />
-                <GithubPages/>
-              </Styled.UsedTech>
-              <Styled.Paragraph>
-              With this app the user can search through millions of articles from various news sources and blogs, and filter them by selecting different categories.
-              </Styled.Paragraph>
-              <Styled.ProjectLinks>
-                <a 
-                  href="https://github.com/ispoljari/news-digest-app"
-                  onClick={delayLinkOpen}
-                >
-                  View Code
-                </a>
-                <a 
-                  href="https://ispoljari.github.io/news-digest-app/"
-                  onClick={delayLinkOpen}
-                >
-                  View Project
-                </a>
-              </Styled.ProjectLinks>
-            </Col>
-          </Row>
-        </ScrollAnimation>
-        <ScrollAnimation
-          animateIn="fadeInRight"
-          offset={150}
-          duration={0.4}
-          animateOnce
+                View Project
+              </a>
+            </Styled.ProjectLinks>
+          </Col>
+        </Row>
+        <Styled.Hr />
+        <Row
+          mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
         >
-          <Styled.Hr />
-          <Row
-            mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
+          <Col
+            xs={12}
+            mb={{xs:"15px", xl:"0px"}}
+            xl={6}
+            display= "flex"
+            alignItems= "flex-end"
           >
-            <Col
-              xs={12}
-              mb={{xs:"15px", xl:"0px"}}
-              xl={6}
-              display= "flex"
-              alignItems= "flex-end"
+            <img 
+              src={recipesResults} 
+              alt="Recipe builder app screenshot"
+              style={{
+                width:"100%",
+                display: "block",
+                margin: "0 auto",
+                borderRadius: "5px"
+              }}
+            />
+          </Col>
+          <Col
+            xs={12}
+            xl={6}
+            pl={{xl:"30px"}}
+          >        
+            <Styled.SectionHeading>
+              Recipe Builder
+            </Styled.SectionHeading>
+            <Styled.UsedTech
+              display="flex"
+              mt="5px"
             >
-              <img 
-                src={recipesResults} 
-                alt="Recipe builder app screenshot"
-                style={{
-                  width:"100%",
-                  display: "block",
-                  margin: "0 auto",
-                  borderRadius: "5px"
-                }}
-              />
-            </Col>
-            <Col
-              xs={12}
-              xl={6}
-              pl={{xl:"30px"}}
-            >        
-              <ScrollAnimation
-                animateIn="fadeInRight"
-                offset={150}
-                duration={0.4}
-                delay={400}
-                animateOnce
+              <Javascript />
+              <ReactIcon />
+              <Html />
+              <Css />
+              <Heroku />
+            </Styled.UsedTech>
+            <Styled.Paragraph>
+            This app is intended for anyone that needs cooking inspiration. The user can take a picture of the ingredients and the app returns a list of related recipes.
+            </Styled.Paragraph>
+            <Styled.ProjectLinks>
+              <a 
+                href="https://github.com/ispoljari/react-recipe-builder-app"
+                onClick={delayLinkOpen}
               >
-                <Styled.SectionHeading>
-                  Recipe Builder
-                </Styled.SectionHeading>
-              </ScrollAnimation>  
-              <Styled.UsedTech
-                display="flex"
-                mt="5px"
+                View Code
+              </a>
+              <a 
+                href="https://limitless-cove-25974.herokuapp.com/"
+                onClick={delayLinkOpen}
               >
-                <Javascript />
-                <ReactIcon />
-                <Html />
-                <Css />
-                <Heroku />
-              </Styled.UsedTech>
-              <Styled.Paragraph>
-              This app is intended for anyone that needs cooking inspiration. The user can take a picture of the ingredients and the app returns a list of related recipes.
-              </Styled.Paragraph>
-              <Styled.ProjectLinks>
-                <a 
-                  href="https://github.com/ispoljari/react-recipe-builder-app"
-                  onClick={delayLinkOpen}
-                >
-                  View Code
-                </a>
-                <a 
-                  href="https://limitless-cove-25974.herokuapp.com/"
-                  onClick={delayLinkOpen}
-                >
-                  View Project
-                </a>
-              </Styled.ProjectLinks>
-            </Col>
-          </Row>
-        </ScrollAnimation>
-        <ScrollAnimation
-          animateIn="fadeInLeft"
-          offset={150}
-          duration={0.4}
-          animateOnce
+                View Project
+              </a>
+            </Styled.ProjectLinks>
+          </Col>
+        </Row>
+        <Styled.Hr />
+        <Row
+          mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
         >
-          <Styled.Hr />
-          <Row
-            mb={{xs:"30px", sm:"40px", lg:"50px", xl:"80px"}}
+          <Col
+            xs={12}
+            mb={{xs:"15px", xl:"0px"}}
+            xl={6}
+            display= "flex"
+            alignItems= "flex-end"
           >
-            <Col
-              xs={12}
-              mb={{xs:"15px", xl:"0px"}}
-              xl={6}
-              display= "flex"
-              alignItems= "flex-end"
+            <img 
+              src={quizzOpeningPage} 
+              alt="Soundtrack quizz app opening page screenshot"
+              style={{
+                width:"100%",
+                display: "block",
+                margin: "0 auto",
+                borderRadius: "5px"
+              }}
+            />
+          </Col>
+          <Col
+            xs={12}
+            xl={6}
+            pl={{xl:"30px"}}
+          >        
+            <Styled.SectionHeading>
+              Soundtracks Quizz
+            </Styled.SectionHeading>
+            <Styled.UsedTech
+              display="flex"
+              mt="5px"
             >
-              <img 
-                src={quizzOpeningPage} 
-                alt="Soundtrack quizz app opening page screenshot"
-                style={{
-                  width:"100%",
-                  display: "block",
-                  margin: "0 auto",
-                  borderRadius: "5px"
-                }}
-              />
-            </Col>
-            <Col
-              xs={12}
-              xl={6}
-              pl={{xl:"30px"}}
-            >        
-              <ScrollAnimation
-                animateIn="fadeInLeft"
-                offset={150}
-                duration={0.4}
-                delay={400}
-                animateOnce
+              <Javascript />
+              <Jquery />
+              <Html />
+              <Css />
+              <GithubPages/>
+            </Styled.UsedTech>
+            <Styled.Paragraph>
+              A quiz consisting of 10 questions. The topic is soundtrack music from movies, tv-shows and games.
+            </Styled.Paragraph>
+            <Styled.ProjectLinks>
+              <a 
+                href="https://github.com/ispoljari/quiz-app"
+                onClick={delayLinkOpen}
               >
-                <Styled.SectionHeading>
-                  Soundtracks Quizz
-                </Styled.SectionHeading>
-              </ScrollAnimation>  
-              <Styled.UsedTech
-                display="flex"
-                mt="5px"
+                View Code
+              </a>
+              <a 
+                href="https://ispoljari.github.io/quiz-app/"
+                onClick={delayLinkOpen}
               >
-                <Javascript />
-                <Jquery />
-                <Html />
-                <Css />
-                <GithubPages/>
-              </Styled.UsedTech>
-              <Styled.Paragraph>
-                A quiz consisting of 10 questions. The topic is soundtrack music from movies, tv-shows and games.
-              </Styled.Paragraph>
-              <Styled.ProjectLinks>
-                <a 
-                  href="https://github.com/ispoljari/quiz-app"
-                  onClick={delayLinkOpen}
-                >
-                  View Code
-                </a>
-                <a 
-                  href="https://ispoljari.github.io/quiz-app/"
-                  onClick={delayLinkOpen}
-                >
-                  View Project
-                </a>
-              </Styled.ProjectLinks>
-            </Col>
-          </Row>
-        </ScrollAnimation>
+                View Project
+              </a>
+            </Styled.ProjectLinks>
+          </Col>
+        </Row>
         <ScrollAnimation
-          animateIn="fadeInUp"
-          offset={50}
+          animateIn="fadeIn"
+          offset={10}
           duration={0.4}
           animateOnce
         >
